fcb2de640ea208487810609db70e6325ff863896
sjs <sspitz14@yahoo.com>
2020-11-03 13:40:43 -0800

    loaded voter data from Julia Silge's repository

--- data/raw/load_data.R
+++ data/raw/load_data.R
@@ -1,1 +1,18 @@
 ### Read data and perform basic data cleaning
+
+library (readr)
+
+urlfile="https://raw.githubusercontent.com/juliasilge/supervised-ML-case-studies-course/master/data/voters.csv"
+# urlfile="https://raw.githubusercontent.com/lrjoshi/webpage/master/public/post/c159s.csv"
+voter.df <- read_csv(url(urlfile))
+
+str(voter.df)
+dim(voter.df)
+
+
+# turnout16_2016 is the response variable
+# it's highly unbalanced
+names(voter.df)[grepl("6_2016", names(voter.df))]
+with(voter.df, table(turnout16_2016, useNA = 'ifany'))
+
+
